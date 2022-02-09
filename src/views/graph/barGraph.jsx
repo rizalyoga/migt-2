bb5ca4a261fa9d34e621a691429b8f7e58a2eab7
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-02-09 20:53:51 +0700

    fix bar chart

--- src/views/graph/barGraph.jsx
+++ src/views/graph/barGraph.jsx
@@ -1,15 +1,48 @@
+import { useState, useEffect } from "react";
 import "./barGraph.css";
 import { Chart as ChartJs, BarElement, CategoryScale, LinearScale } from "chart.js";
 import { Bar } from "react-chartjs-2";
 
 ChartJs.register(BarElement, CategoryScale, LinearScale);
+
 const BarGraph = () => {
+  const [chart, setCharts] = useState([]);
+
+  const baseURL = `https://api.coinranking.com/v2/coins/?limit=10`;
+  const proxyURL = "https://cors-anywhere.herokuapp.com/";
+  const apiKey = process.env.REACT_APP_MY_KEY;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(`${proxyURL}${baseURL}`, {
+        method: `GET`,
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": `${apiKey}`,
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+        .then((response) => {
+          response.json().then((json) => {
+            setCharts(json.data);
+            // console.log(chart);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetchData();
+    console.log(chart);
+  }, [baseURL, proxyURL, apiKey]);
+
   let data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: chart?.coins?.map((el) => el.name),
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 11, 3, 5, 2, 3],
+        label: `${chart?.coins?.length} Coins Available`,
+        data: chart?.coins?.map((el) => el.price),
         backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(255, 159, 64, 0.2)"],
         borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)", "rgba(255, 159, 64, 1)"],
         borderWidth: 1,
